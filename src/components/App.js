20e99c4232a2e6de6612f9e2d6c18f252c56b8aa
KADYAN <35776003+unique-kadyan@users.noreply.github.com>
2021-08-18 03:31:33 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,41 @@
 import React, { Component, useState, useEffect } from "react";
-import '../styles/App.css';
+import "../styles/App.css";
 
-const App = () => {
-  // write your code here 
+export default function App() {
+  const [currentTime, setCurrentTime] = useState(0);
+
+  const reverseCount = (event, inputValue) => {
+    if (event.keyCode !== 13) return;
+    inputValue = Math.floor(inputValue);
+    if (inputValue < 0) {
+      inputValue = 0;
+    }
+
+    setCurrentTime(inputValue);
+  };
+
+  useEffect(() => {
+    let id = null;
+    if (currentTime > 0) {
+      id = setInterval(() => setCurrentTime(currentTime - 1), 1000);
+    }
+    return () => clearInterval(id);
+  });
 
   return (
     <div className="wrapper">
       <div id="whole-center">
         <h1>
-          Reverse countdown for<input id="timeCount" onKeyDown={/* callback function */} /> sec.
+          Reverse countdown for
+          <input
+            id="timeCount"
+            type="number"
+            onKeyDown={(e) => reverseCount(e, e.target.value)}
+          />{" "}
+          sec.
         </h1>
       </div>
-      <div id="current-time">{/* remaining time */}</div>
+      <div id="current-time">{currentTime}</div>
     </div>
-  )
+  );
 }
-
-
-export default App;
